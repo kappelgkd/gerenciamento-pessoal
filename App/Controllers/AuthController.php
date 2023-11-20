41036c9b66ea866839becf7b748eab185a3a564d
Guilherme <g.dinamarco@gmail.com>
2023-11-19 21:38:47 -0300

    realizar login via ajax e gerar um token aleatorio do lado do servidor

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -14,8 +14,8 @@ class AuthController extends Action{
 
         $usuario->__set('login', $_POST['login']);
         $usuario->__set('senha', $_POST['senha']);
-       
-    
+        $token = bin2hex(random_bytes(32));
+        $retorno = array();
         $usuario->autenticar();
         
         // validando se as informações foram preenchidas para validar a autenticacao e realizar o direcionamento.
@@ -25,13 +25,24 @@ class AuthController extends Action{
             session_start();
             $_SESSION['id'] = $usuario->__get('id');
             $_SESSION['nome'] = $usuario->__get('nome');
+            $retorno=array(
+                "status" => "200",
+                "redirecionar"=>"dashboard",
+                "mensagem"=>"Login efetuado",
+                "token"=>$token
 
-            header('Location: /dashboard', '200');
+            );
+            //header('Location: /dashboard', '200');
         }
         else{
-           header('Location: /?login=erro', '400');
+           //header('Location: /?login=erro', '400');
+            $retorno = array(
+                "status" => "404",
+                "mensagem"=>"Erro ao efetuar login"
+            );
         }
-
+        
+        echo json_encode($retorno,true);
     }
-
+    
 }
--- App/Views/index/index.php
+++ App/Views/index/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página de Login</title>
-    
+
 </head>
+
 <body>
     <div class="container">
         <div class="row">
@@ -20,11 +22,46 @@
                         <label for="password">Senha:</label>
                         <input type="password" class="form-control" id="password" placeholder="Informe sua senha" name="senha">
                     </div>
-                    <button type="submit" class="btn btn-purple btn-block">Entrar</button>
+                    <button type="button" id="btnAutenticar" class="btn btn-purple btn-block">Entrar</button>
                 </form>
                 <p class="text-center mt-3"><a href="#">Primeiro Acesso?</a></p>
             </div>
         </div>
     </div>
 </body>
+
 </html>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+    const btnAutenticar = $("#btnAutenticar");
+    btnAutenticar.on("click", function() {
+        //console.log("inicio");
+        var login = $("#username").val();
+        var senha = $("#password").val();
+
+        if (login != "" && senha != "") {
+            var dados = {
+                login: login,
+                senha: senha
+            };
+            $.ajax({
+                type: "POST",
+                url: "/autenticar",
+                data: dados,
+                success: function(response) {
+                    console.log(response);
+
+                    if (response.status = 200) {
+                        console.log("aqui");
+                        window.location.href = "/dashboard"
+                    }
+                },
+                error: function() {
+                    // em caso de erro
+                    alert("100 - Tente novamente mais tarde.");
+                }
+            });
+        }
+    })
+</script>
