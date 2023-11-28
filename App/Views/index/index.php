efed0e532801f797774af0fdaf8d2c1ee9bead4c
Guilherme <g.dinamarco@gmail.com>
2023-11-28 07:55:29 -0300

    respnsividade no login

--- App/Views/index/index.php
+++ App/Views/index/index.php
@@ -11,7 +11,7 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-6 offset-md-3 login-form">
+            <div class="col-md-6 col=sm-1 offset-md-3 login-form">
                 <h2 class="text-center">Login</h2>
                 <form method="POST" action="/autenticar">
                     <div class="form-group">
@@ -54,7 +54,7 @@
                     console.log(response);
                     if (response.status = 200) {
                         console.log("aqui");
-                        window.location.href = "/dashboard"
+                        //window.location.href = "/dashboard"
                     }
                 },
                 error: function() {
